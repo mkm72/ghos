e2040a6d40664f626bca7b12d49ea60af654ab5b
HabibAlShaikh <ihabibshaikh@gmail.com>
2026-04-30 22:57:49 +0300

    fix Error database location?

--- cart.php
+++ cart.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
 }
-require_once 'db.php';
+require_once 'database/ghos.php';
 
 $stmt = $pdo->prepare("
     SELECT Cart.id AS cart_id, Cart.quantity,
