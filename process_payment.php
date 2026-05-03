d955e968edaf11f8c5650a3ae3dea05f1e6ae5ca
mkm72 <mkm7294@proton.me>
2026-05-03 08:32:48 +0300

    update

--- process_payment.php
+++ process_payment.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-s
+
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header('Location: auth.php');
