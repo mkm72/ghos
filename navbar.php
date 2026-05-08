453874d14aee110c68b515c3a5838c567552b292
alhashem.ali <74969561+1HerOz@users.noreply.github.com>
2026-05-08 10:14:46 +0300

    Change contact link to use JavaScript void

--- navbar.php
+++ navbar.php
@@ -32,7 +32,7 @@ if ($is_logged_in) {
     </div>
     
     <div class="navbar-links">
-        <a href="#" class="contact-link">📞 Contact Us</a>
+        <a href="javascript:void(0)" class="contact-link">📞 Contact Us</a>
 
         <select id="currencySelector" class="currency-select">
             <option value="USD">USD ($)</option>
